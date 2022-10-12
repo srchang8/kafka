1abac55c520c52e70de12333abfe2a325ab73fb6
srchang8 <srchang8@gmail.com>
2022-10-12 03:33:18 -0400

    Stephen/base (#3)
    * initialm app creation
    
    * add model and controller
    
    * fix open api dependency issue
    
    * add kafka docker instance
    
    * commands for kafka
    
    * add PR template
    
    * fix pr template
    
    * add kafka client
    
    * add kafka producer
    
    * config changes
    
    * added consumer
    
    Co-authored-by: Alex Hoang <alexhoang2@gmail.com>

---
+++ kafkaApp/src/main/java/Producer.java
@@ -0,0 +1,31 @@
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class Producer {
+    public static void main(String[] args) {
+
+        final Logger logger = LoggerFactory.getLogger(Producer.class);
+
+        // Create properties object for Producer
+        Properties prop = new Properties();
+        prop.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        prop.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        prop.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // Create the Producer
+        final KafkaProducer<String, String> producer = new KafkaProducer<String, String>(prop);
+
+        ProducerRecord<String, String> record = new ProducerRecord<>("SteAlexTopic", "key1", "value1");
+
+        producer.send(record);
+
+        producer.flush();
+        producer.close();
+
+
+    }
+}
